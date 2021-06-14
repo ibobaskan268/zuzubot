9606b9eb8eb26ff633d95a3603e1167f827420b7
ibobaskan268 <61876538+ibobaskan268@users.noreply.github.com>
2021-06-14 15:34:50 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+ setInterval(function() {
+}, 8000);
+  var msgArray = [
+`${prefix}yardım`,`Zuzu`
+ ];
+
+ setInterval(() => {
+  var rastgeleOyun = Math.floor(Math.random() * msgArray.length);
+  client.user.setActivity(`${msgArray[rastgeleOyun]}`, { type: 'STREAMING' ,  url: 'https://www.twitch.tv/mbasreaper' })
+}, 5000);
+    console.log(`:.:.: Hazır `);
+}
